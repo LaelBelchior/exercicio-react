ee863e541ba705c5b79bb80542e3c96bdf6eac4c
LaelBelchior <laelbelchior@gmail.com>
2022-05-23 21:14:38 -0300

    Criando component lista-notas.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import ListaNotas from './Components/lista-notas';
 
 function App() {
   return (
-    <h1>Ola, mundo!</h1>
+    <section>
+      <form>
+        <input type="text"/>
+        <textarea placeholder='Escreva sua nota...'/>
+        <button>ENVIAR NOTA</button>
+      </form>
+      <ListaNotas />
+    </section>
   );
 }
 
