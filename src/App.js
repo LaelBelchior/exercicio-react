78d2c2bb441ccda66b5c63e3011711ad4e33d1f6
LaelBelchior <laelbelchior@gmail.com>
2022-05-28 21:39:47 -0300

    Alterando app.js para usar this.state

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ class App extends Component{
 
   constructor(){
     super()
-    this.notas = []
+    this.state = {
+      notas: []
+    }
   }
 
   criarNota(titulo, texto){
     const novaNota = {titulo, texto}
-    this.notas.push(novaNota)
+    const novoArray = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArray
+    }
+    this.setState(novoEstado)
   }  
 
   render(){
     return (
       <section className='conteudo'>
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaNotas notas={this.notas}/>
+        <ListaNotas notas={this.state.notas}/>
       </section>
     );
   }
